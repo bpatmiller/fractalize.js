3aaf6e5a755cc42d8911a253b07f01f830e153a3
Brendan Miller <brendanmillerca@gmail.com>
2021-07-06 22:37:24 -0700

    deploy

--- src/index.js
+++ src/index.js
@@ -3,19 +3,28 @@ import { makePane, PARAMS } from "./config.js";
 import { setupGL } from "./gl.js";
 const [pane, fpsGraph] = makePane();
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const imgParamUrl = urlParams.get("img");
+
 const init = async (file) => {
+  let ipu = null;
+  if (file == null) {
+    ipu = imgParamUrl;
+  }
+
   const [lejaStack, A_nStack, centers, colors, setSizes] = await handleImage(
     file,
-    imgParamUrl
+    ipu
   );
   setupGL(lejaStack, A_nStack, centers, colors, setSizes, fpsGraph);
 };
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-
-const imgParamUrl = urlParams.get("img");
 const nC = urlParams.get("nc");
+const nL = urlParams.get("nl");
+if (nL != null) {
+  PARAMS.numLejaPoints = parseInt(nL);
+}
 if (nC != null) {
   PARAMS.numColors = parseInt(nC);
 }
@@ -23,7 +32,18 @@ if (imgParamUrl != null) {
   init(null, imgParamUrl);
 }
 
-var dropZone = document.getElementById("drop");
+const dropZone = document.getElementById("drop");
+
+const clickContainer = document.getElementById("fractal-container");
+
+clickContainer.addEventListener("click", (e) => {
+  const fractalView = document.getElementById("fractal");
+  const imageView = document.getElementById("resized");
+  fractalView.classList.toggle("hidden");
+  imageView.classList.toggle("hidden");
+  console.log("e");
+  e.preventDefault();
+});
 
 dropZone.addEventListener("dragover", function (e) {
   this.classList.add("dragging");
